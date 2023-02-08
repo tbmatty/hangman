0e3eef81e2026c521f540f5b88a852fdab82aa7d
theba <thebaddestmatty@outlook.com>
2023-02-08 20:26:54 +0000

    milestone 4

--- milestone_3.py
+++ milestone_3.py
@@ -1,35 +1,27 @@
-#%%
 import random
-word_list = ['mango','blueberries','banana','pineapple','watermelon']
-word = random.choice(word_list)
+
+if __name__ == "__main__":
+    word_list = ['mango','blueberries','banana','pineapple','watermelon']
+    word = random.choice(word_list)
 
 
-
-#%%
-import random
-word_list = ['mango','blueberries','banana','pineapple','watermelon']
-word = random.choice(word_list)
-
-
-def check_guess(guess):
-    guess = guess.lower()
-    if(guess.isalpha() and len(guess)==1):
-        if(guess in word):
-            print("Good guess! "+guess+" is in the word")
-            return
-        else:
-            print("Uh oh! That is not in the word")
-    else:
-        print("Oops! That's not a valid guess. Please enter a single alphabetical character. You're playing hangman!")
-
-def ask_for_input():
-    while(True):
-        guess = input("Please pick a letter")
+    def check_guess(guess):
+        guess = guess.lower()
         if(guess.isalpha() and len(guess)==1):
-            break
+            if(guess in word):
+                print("Good guess! "+guess+" is in the word")
+                return
+            else:
+                print("Uh oh! That is not in the word")
         else:
             print("Oops! That's not a valid guess. Please enter a single alphabetical character. You're playing hangman!")
-    check_guess(guess=guess)
 
-ask_for_input()
-# %%
+    def ask_for_input():
+        while(True):
+            guess = input("Please pick a letter")
+            if(guess.isalpha() and len(guess)==1):
+                break
+            else:
+                print("Oops! That's not a valid guess. Please enter a single alphabetical character. You're playing hangman!")
+        check_guess(guess=guess)
+
---
+++ milestone_4.py
@@ -0,0 +1,38 @@
+import random
+class Hangman:
+
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for i in range(len(self.word))]
+        self.num_letters = len(list(set(self.word)))
+        self.list_of_guesses = []
+    
+    def check_guess(self,guess):
+        guess = guess.lower()
+        word_chars = self.word.split('')
+
+        if(guess in self.word):
+            print("Good guess! "+guess+" is in the word")
+            for i in range(len(word_chars)):
+                if(guess==word_chars[i]):
+                    self.word_guessed[i]=guess
+                    
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print("Uh oh! That is not in the word")
+            print("You have "+str(self.num_lives)+" lives left")
+        self.list_of_guesses.append(guess)
+        return 
+
+    def ask_for_input(self):
+        while(True):
+            guess = input("Please pick a letter")
+            if(not(guess.isalpha() and len(guess)==1)):
+                print("Invalid letter. What the heck! Enter a single alphabetical character")
+            elif(guess in self.list_of_guesses):
+                print("Oops! You silly goose. You've already guessed that letter.")
+            else:
+                self.check_guess(guess)
